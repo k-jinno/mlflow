8b77f0627874312a1149263f54f356ea9cc1d839
jinno <kjinno.rikada@gmail.com>
2022-07-24 16:41:14 +0900

    first commit

---
+++ main.py
@@ -0,0 +1,17 @@
+import mlflow
+mlflow.start_run()
+
+# Log a parameter (key-value pair)
+mlflow.log_param("param1", 5)
+
+# Log a metric; metrics can be updated throughout the run
+mlflow.log_metric("foo", 2, step=1)
+mlflow.log_metric("foo", 4, step=2)
+mlflow.log_metric("foo", 6, step=3)
+
+# Log an artifact (output file)
+with open("output.txt", "w") as f:
+    f.write("Hello world!")
+mlflow.log_artifact("output.txt")
+
+mlflow.end_run()
